66facd9c42dfcf90ec564dd5e503eaa7361bcaaf
oGronman <zaoshu@DESKTOP-QHA8IL2.localdomain>
2021-09-13 09:15:38 +0200

    Restructure code for faster time

--- summera-tal/src/main.rs
+++ summera-tal/src/main.rs
@@ -1,53 +1,33 @@
 use std::io;
-use std::io::prelude::*;
 
-/// Kattis calls main function to run your solution.
 fn main() {
-    // get standard input stream
-    let input = io::stdin();
 
-    // get input lines as strings
-    // see: https://doc.rust-lang.org/std/io/trait.BufRead.html
+        let mut nmr_times = String::new();
+        io::stdin().read_line(&mut nmr_times).expect("Error");
+        let nmr_times: usize = nmr_times.trim().parse().expect("Error");
+        
 
-    let mut lines = input
-        .lock()
-        .lines()
-        .map(|_line| _line.ok().unwrap());
-
-
-    let nmr_times = lines
-    .next().unwrap()
-    .trim()
-    .parse::<usize>().ok().expect("Error"); 
-    
-    let add_nmr;
-    if nmr_times%2 == 0{
-        add_nmr = nmr_times/2;
-        eprintln!("Is even");
-    }else{
-        add_nmr = (nmr_times+1)/2-1;
-        eprintln!("Is odd");
-    }
-
-    eprintln!("{}", add_nmr);
-
-        let mut numbers = lines
-            .next().expect("Error")
-            .trim()
-            .split(" ")
+        let mut numbers = String::new();
+        io::stdin().read_line(&mut numbers).expect("Error");
+        let mut numbers: Vec<u32> = numbers.trim().split(" ")
             .map(|component| component.parse::<u32>().ok().expect("Error"))
             .collect::<Vec<u32>>();
-    
+
         numbers.sort();
 
+        eprintln!("{}", nmr_times);
+
         let mut result: u32 = 0;
-
-        for _times in add_nmr..nmr_times{
-            result = result + numbers[_times];
-            eprintln!("{}", numbers[_times])
+        if nmr_times%2 == 0{
+            eprintln!("Even");
+            for _index in (nmr_times/2)..nmr_times{
+                result += numbers[_index];
+            }
+        }else{
+            for _index in ((nmr_times+1)/2-1)..nmr_times{
+                result += numbers[_index];
+            }
         }
-    
         println!("{}", result);
-
 
 }
